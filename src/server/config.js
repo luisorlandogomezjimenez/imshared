ed0c329448b91d02a205c3d0300f9295f874f460
Orlando <luisorlandogomezjimenez@gmail.com>
2021-07-27 18:02:45 -0500

    express config

--- src/server/config.js
+++ src/server/config.js
@@ -1,3 +1,14 @@
+const path = require("path");
+
 module.exports = (app) => {
+  //Settings
+  app.set("port", process.env.PORT || 3000);
+  app.set("views", path.join(__dirname, "views"));
+  //middlewares
+
+  //routes
+
+  //errorhandlers
+
   return app;
 };
