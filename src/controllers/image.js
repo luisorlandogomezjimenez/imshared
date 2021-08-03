57c1662b2e088670d0400d7f225f3147d4cfb9d7
Orlando <luisorlandogomezjimenez@gmail.com>
2021-08-03 10:56:03 -0500

    router

---
+++ src/controllers/image.js
@@ -0,0 +1,13 @@
+ctrl = {};
+
+ctrl.index = (req, res) => {};
+
+ctrl.create = (req, res) => {};
+
+ctrl.like = (req, res) => {};
+
+ctrl.comment = (req, res) => {};
+
+ctrl.remove = (req, res) => {};
+
+module.exports = ctrl;
