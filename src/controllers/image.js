e4dd45b86e4e362d723bd98af4b315a8a1b9e5f9
Orlando <luisorlandogomezjimenez@gmail.com>
2021-08-13 12:13:41 -0500

    controllers, models, moongose

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -1,8 +1,35 @@
+const path = require("path");
+const { randomNumber } = require("../helpers/libs");
+const fs = require("fs-extra");
+
+const { Image } = require("../models");
+
 ctrl = {};
 
 ctrl.index = (req, res) => {};
 
-ctrl.create = (req, res) => {};
+ctrl.create = async (req, res) => {
+  const imgUrl = randomNumber();
+  console.log(imgUrl);
+
+  const imageTempPath = req.file.path;
+  const ext = path.extname(req.file.originalname).toLowerCase();
+  const targetPath = path.resolve(`src/public/upload/${imgUrl}${ext}`);
+
+  if (ext === ".png" || ext === ".jpg" || ext === ".jpeg" || ext === ".gif") {
+    await fs.rename(imageTempPath, targetPath);
+
+    const newImg = new Image({
+      title: req.body.title,
+      filename: imgUrl + ext,
+      description: req.body.description,
+    });
+    const imgSaved = await newImg.save();
+    console.log(newImg);
+  }
+
+  res.send("Works!");
+};
 
 ctrl.like = (req, res) => {};
 
