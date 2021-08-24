9de9d18b6add6459a919ad77cadfedd5814aada9
Orlando <luisorlandogomezjimenez@gmail.com>
2021-08-23 21:07:29 -0500

    sidebar and finish

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -4,11 +4,12 @@ const fs = require("fs-extra");
 const md5 = require("md5");
 
 const { Image, Comment } = require("../models/index");
+const sidebar = require("../helpers/sidebar");
 
 ctrl = {};
 
 ctrl.index = async (req, res) => {
-  const viewModel = { image: {}, comments: {} };
+  let viewModel = { image: {}, comments: {} };
   const image = await Image.findOne({
     filename: { $regex: req.params.image_id },
   });
@@ -18,6 +19,7 @@ ctrl.index = async (req, res) => {
     await image.save();
     const comments = await Comment.find({ image_id: image._id });
     viewModel.comments = comments;
+    viewModel = await sidebar(viewModel);
     res.render("image", viewModel);
   } else {
     res.redirect("/");
